01de24d8644e45d7ab4b2f0bab0a2bf6df3dd99e
user <user@deb>
2016-04-01 17:52:22 +0200

    beta

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ curl -X POST --data-binary @/tmp/config.txt http://vps260977.ovh.net:80
 ### get file  
 curl http://vps260977.ovh.net/p/TOKEN > file.txt
 
+use header "Action: delete" in curl to delete file on the server  
+curl -s -H "Action: delete" http://vps260977.ovh.net/p/TOKEN > file.txt  
+
 # script exemple (openssl + curl)  
 ``./shellpaste.sh -s /tmp/test.txt  
 http://vps260977.ovh.net/p/xc5Zv9poQG
@@ -24,7 +27,7 @@ get the file
 ``
 ./shellpaste.sh -g xc5Zv9poQG
 ``  
-file save in myfile.txt
+file save in myfile.txt and remove on the server (use -n if you need to keep it)
 
 
 *thanks http://dillinger.io/ to format readme* 
--- shellpaste.sh
+++ shellpaste.sh
@@ -9,6 +9,7 @@
 # -h : help
 # -s : send file (-s path/to/file)
 # -g : get file (-g token)
+# -n : get file but nodelete
 
 # openssh password 
 secret="3d1tMeF0rS3kurP4ss"
@@ -47,15 +48,20 @@ function sendFile {
 function getFile {
 
 	value=$1
+	action=$2 # if action no delete
 	if [ ! -z $value ]; then
-		curl -s -o $tmpFile ${getUrl}${value}
-		if [ -e $tmpFile ]; then
-			openssl enc $enc -salt -a -d -in $tmpFile -out myFile.txt -pass pass:${secret}
-			rm $tmpFile
+		if [ -z $action ]; then
+			curl -s -H "Action: delete" -o $tmpFile ${getUrl}${value}
 		else
-			echo "no temporary file writen ..."
-			exit 4
+			curl -s -o $tmpFile ${getUrl}${value}
 		fi
+			if [ -e $tmpFile ]; then
+				openssl enc $enc -salt -a -d -in $tmpFile -out myFile.txt -pass pass:${secret}
+				rm $tmpFile
+			else
+				echo "no temporary file writen ..."
+			exit 4
+			fi
 	else
 		echo "empty token ..."
 		exit 3
@@ -68,14 +74,15 @@ function help {
 	echo "$0 [ -h | -s path/to/file | -g token ]"
 	echo " -s to send a file"
 	echo " -g to get a file from the token (file is write in myfile.txt)"
+	echo " -n same as -g but don't delete file on the serveur"
 	echo "file is remove after download it"
 	echo "you can use the service without the script:"
-	echo " $ curl -X POST --data-binary @/home/daniel/config.txt http://$server:$port"
+	echo " $ curl -X POST --data-binary @/tmp/config.txt http://$server:$port"
 	echo " $ curl http://$server:$port/p/TOKEN > myfile.txt"
 	echo ""
 }
 
-while getopts ":s:g:h" option; do
+while getopts "s:g:n:h" option; do
 
 	case $option in
 	s)
@@ -83,6 +90,9 @@ while getopts ":s:g:h" option; do
 	 	;;
 	g)
 	 	getFile $OPTARG
+		;;
+	n)
+		getFile $OPTARG "nodelete"
 	  	;;
 	h)
 	  	help
